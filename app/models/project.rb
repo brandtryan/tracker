d51451a42779f6226f9f38c54a7c0b7510379848
Brandt Ryan <emailme@brandtryan.com>
2014-05-14 05:50:42 -0400

    thru validation exercise

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,9 @@ class Project < ActiveRecord::Base
     has_many :works
     has_many :users, :through => :works
 
+    validates :name, :company, presence: true
+    validates :name, length: { minimum: 5 }
+    validates :default_rate, numericality: { only_integer: true, greater_than_or_equal_to: 50 }
+
     scope :lowdefaultrate, -> { where("default_rate < 100") }
 end
