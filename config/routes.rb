87a038261f7b686922d47dfdf355fe4819742c53
Brandt Ryan <emailme@brandtryan.com>
2014-05-14 08:02:22 -0400

    Generating Controllers Exercise

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Tracker::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :companies
+  resources :projects
+  resources :works
+
   # Example resource route with options:
   #   resources :products do
   #     member do
